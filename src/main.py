03855a43fb91f2aa26eeb06cab061201582bbabb
Charles <46151389+DaddyChucky@users.noreply.github.com>
2022-08-09 16:57:07 -0400

    Completed draft script

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ from loadcsts import *
 from time import sleep
 from random import randint as ri
 from collections import deque
+import json
 
 def humanize_action():
     MIN_MS      = 300
@@ -19,11 +20,11 @@ def humanize_action():
     MS_FACTOR   = 1000
     sleep(ri(MIN_MS, MAX_MS) / MS_FACTOR)
 
-def start():
-    page = POLYMTL_PAGE
+def start(page: str):
     DRIVER.get(page)
     RELATED_PAGE    = page[MIN_RELATED_LINK:]
     VISITED_LINKS   = set()
+    VISITED_LINKS.add(page)
     humanize_action()
     CONTENT = {}
 
@@ -44,31 +45,34 @@ def start():
 
         # Breadth First Search (BFS) Algorithm
         while len(upcoming_links) > 0:
+            print(len(upcoming_links))
             current_link = upcoming_links.popleft()
             
-            if last_link == current_link or ANTI_ANCHOR in current_link or RELATED_PAGE not in current_link or current_link in VISITED_LINKS: continue
-            
+            if current_link in VISITED_LINKS or ANTI_ANCHOR in current_link or RELATED_PAGE not in current_link or last_link == current_link: continue
+
             VISITED_LINKS.add(current_link)
             print("Currently visiting link:", current_link)
             
             DRIVER.get(current_link)
-            humanize_action()
 
             page_links = DRIVER.find_elements(By.XPATH, ALL_LINKS_XPATH)
-            humanize_action()
 
             for page_link in page_links:
-                upcoming_links.append(page_link.get_attribute(LINK_ATTRIBUTE))
+                try:
+                    upcoming_links.append(page_link.get_attribute(LINK_ATTRIBUTE))
+                except Exception:
+                    continue
 
             CONTENT[current_link] = DRIVER.find_element(By.XPATH, BODY_XPATH).text
-            print("Saved", len(CONTENT[current_link]), "characters. Continuing.")
-
-            humanize_action()
+            print("Saved", len(CONTENT[current_link]), "characters for", current_link, "Continuing...")
 
             last_link = current_link
+
+            with open(os.getcwd() + r"\out\data.json", "w+", encoding="utf-8") as f:
+                json.dump(CONTENT, f, ensure_ascii=True, check_circular=True, allow_nan=True, indent=4)
         
     except Exception as e:
         print("Fatal error:", e)
 
 if __name__ == "__main__":
-    start()
+    start(POLYMTL_PAGE)
